d886fca91a520c239d262f053be952002446a3bb
Amina Rahman Ananna <aminaananna@yahoo.com>
2017-08-01 09:08:35 +0600

    Create priority queue implementation.cpp

---
+++ priority queue implementation.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    priority_queue <int> PQ;
+    PQ.push(10);
+    PQ.push(25);
+    PQ.push (18);
+    
+    cout << PQ.top() << endl;
+    return 0;
+}
